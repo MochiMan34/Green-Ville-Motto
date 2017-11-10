047820f9466f232d7a6a4e8b030c872bc293ccc3
MochiMan34 <st27567@email.vccs.edu>
2017-11-10 15:47:58 -0500

    Add files via upload

---
+++ Greenville Motto/Case1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case1
+{
+    
+    class Program
+    {
+        public static String greenvilleText = "The stars shine in Greenville.";
+        public static String BottomBorder = "";
+        public static String topBorder = "";
+        
+        
+
+
+        static void Main(string[] args)
+        {
+            greenvilleMotto();
+            System.Console.WriteLine(" ");
+            greenvilleMotto2(); 
+           
+
+
+            System.Console.ReadKey();
+        }
+
+        public static void greenvilleMotto()
+        {
+            System.Console.WriteLine(greenvilleText);   
+            
+
+
+        }
+
+        public static void greenvilleMotto2()
+        {
+            generateAsteriskBorder(topBorder);
+            System.Console.WriteLine("*" + greenvilleText + "*");
+            generateAsteriskBorder(BottomBorder);
+        }
+
+        public static void generateAsteriskBorder(String text)
+        {
+            for(int i = 0; i < greenvilleText.Length+2; i++)
+            {
+                text += "*"; 
+            }
+            System.Console.WriteLine(text); 
+        }
+
+       
+
+      
+
+      
+    }
+}
